9d6c84fc60549e71424ee0b6e66b0ed7247137ba
Andrey <steamprofile2020@gmail.com>
2021-11-25 21:32:49 +0300

    changing the size of columns

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,5 +1,6 @@
 import { ExcelComponent } from '@core/ExcelComponent'
 import { createTable } from '@/components/table/table.template'
+import { $ } from '@core/DOM'
 
 export class Table extends ExcelComponent {
     static className = 'excel__table'
@@ -16,7 +17,19 @@ export class Table extends ExcelComponent {
 
     onMousedown(event) {
         if (event.target.dataset.resize) {
-            console.log(event.target.dataset.resize)
+            const $resizer = $(event.target)
+            const $parent = $resizer.closest('[data-type="resizable"]')
+            const coords = $parent.getCoords()
+
+            document.onmousemove = e => {
+                const delta = e.pageX - coords.right
+                const value = coords.width + delta
+                $parent.$el.style.width = value + 'px'
+            }
+
+            document.ommouseup = () => {
+                document.onmousemove = null
+            }
         }
     }
 }
