d11d3846989d47ccf490dd4047670841176c11b9
Andrey <steamprofile2020@gmail.com>
2021-12-06 19:32:19 +0300

    frontend: a bundle of store and ui

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -36,6 +36,10 @@ export class Table extends ExcelComponent {
         this.$on('formula:done', () => {
             this.selection.current.focus()
         })
+
+        this.$subscribe(state => {
+            console.log('TableState', state)
+        })
     }
 
     selectCell($cell) {
@@ -52,7 +56,7 @@ export class Table extends ExcelComponent {
                 const $cells = matrix($target, this.selection.current).map(id => this.$root.find(`[data-id="${id}"]`))
                 this.selection.selectGroup($cells)
             } else {
-                this.selection.select($target)
+                this.selectCell($target)
             }
         }
     }
