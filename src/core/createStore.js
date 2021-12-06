2e31c4cd9154e4d31349a429f18a21abb7743a39
Andrey <steamprofile2020@gmail.com>
2021-12-06 20:36:24 +0300

    frontend: creating column states

--- src/core/createStore.js
+++ src/core/createStore.js
@@ -13,7 +13,7 @@ export function createStore(rootReducer, initialState = {}) {
         },
         dispatch(action) {
             state = rootReducer(state, action)
-            listeners.forEach(listener = listener(state))
+            listeners.forEach(listener => listener(state))
         },
         getState() {
             return state
