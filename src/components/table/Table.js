fb31f18029e6a1a8c969687f33d17e2e9c36140a
Andrey <steamprofile2020@gmail.com>
2021-12-08 21:13:37 +0300

    frontend: saving state

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -4,6 +4,7 @@ import { createTable } from '@/components/table/table.template'
 import { resizeHandler } from '@/components/table/table.resize'
 import { isCell, matrix, nextSelector, shouldResize } from '@/components/table/table.functions'
 import { TableSelection } from '@/components/table/TableSelection'
+import * as actions from '@/redux/actions'
 
 export class Table extends ExcelComponent {
     static className = 'excel__table'
@@ -36,10 +37,6 @@ export class Table extends ExcelComponent {
         this.$on('formula:done', () => {
             this.selection.current.focus()
         })
-
-        this.$subscribe(state => {
-            console.log('TableState', state)
-        })
     }
 
     selectCell($cell) {
@@ -50,7 +47,7 @@ export class Table extends ExcelComponent {
     async resizeTable(event) {
         try {
             const data = await resizeHandler(this.$root, event)
-            this.$dispatch({ type: 'TABLE_RESIZE', data })
+            this.$dispatch(actions.tableResize(data))
             console.log(data)
         } catch (e) {
             console.warn('Resize error', e.message)
