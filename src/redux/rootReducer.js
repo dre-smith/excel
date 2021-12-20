e3b6b6de4ed58dd260841480aa8ee441c6631fc4
Andrey <steamprofile2020@gmail.com>
2021-12-20 20:04:39 +0300

    frontend: changing the table name

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,4 +1,4 @@
-import { CHANGE_TEXT, CHANGE_STYLES, TABLE_RESIZE, APPLY_STYLE } from './types'
+import { TABLE_RESIZE, CHANGE_TEXT, CHANGE_STYLES, APPLY_STYLE, CHANGE_TITLE } from './types'
 
 export function rootReducer(state, action) {
     let field
@@ -28,6 +28,8 @@ export function rootReducer(state, action) {
                 [field]: val,
                 currentStyles: { ...state.currentStyles, ...action.data.value }
             }
+        case CHANGE_TITLE:
+            return { ...state, title: action.data }
         default: return state
     }
 }
